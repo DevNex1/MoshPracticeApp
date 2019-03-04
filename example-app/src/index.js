741b27e3342d9d468c2a1760e0629d66c263f2b8
DevNex1 <annexta@gmail.com>
2019-03-03 19:47:27 -0800

    no message

---
+++ example-app/src/index.js
@@ -0,0 +1,48 @@
+//examples of ES6
+/* const person = {
+  name: "AD",
+  walk() {},
+  talk() {}
+};
+
+person.talk();
+
+const targetMember = "name";
+person[targetMember.value] = "John"; */
+
+//this exmaple
+/* const person = {
+  name: "AD",
+  walk() {
+    console.log(this);
+  },
+  talk() {}
+};
+
+person.walk();
+walk();
+  */
+
+//es6 error example
+//   const square = function(number) {
+//       return number * number;
+//   }
+
+// const square = number => number * number;
+
+// console.log(square(5));
+
+// const jobs = [
+//   { id: 1, isActive: true },
+//   { id: 2, isActive: true },
+//   { id: 3, isActive: false }
+// ];
+
+// const activejobs = jobs.filter(function(job) {
+//   return job.isActive;
+// });
+
+// const activejobs = jobs.filter(job => job.isActive);
+
+
+
